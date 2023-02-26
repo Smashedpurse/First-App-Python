53b30ab137e8967582d0b41d40a70bcdbd825607
Ivan Rubio <smashedpurse@gmail.com>
2023-02-26 16:27:37 -0600

    Add the diferent ways to add a Variable in Python

---
+++ tipos/01_Variables.py
@@ -0,0 +1,8 @@
+"""Module providingFunction printing python version."""
+NOMBRE_CURSO = "Ultimate Python"
+print(NOMBRE_CURSO)
+N1 = "My First Variable"
+NOMBRE1 = "Ultimate Python"
+print(N1)
+print(NOMBRE1)
+NombreCurso = "Ultimate Python"
